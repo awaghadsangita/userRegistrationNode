b52f01d0c77e979f629a36352d0d79567715caaf
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-28 15:41:57 +0530

    ADDED:code to validate email

--- validator.js
+++ validator.js
@@ -3,5 +3,10 @@ class Validator{
 		let nameRegex=RegExp("^[A-Z]{1}[a-zA-Z]{2,}");
 		return nameRegex.test(name);
 	}
+	
+	validateEmail(email){
+		let emailRegex=RegExp("^[a-zA-Z0-9]([._+-]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}$");
+		return emailRegex.test(email);	
+	}
 }
 module.exports=new Validator();
