e8412e966d45591492c4ded41b541fbe42f39b52
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-28 15:51:57 +0530

    ADDED:code to validate mobile number

--- validator.js
+++ validator.js
@@ -8,5 +8,10 @@ class Validator{
 		let emailRegex=RegExp("^[a-zA-Z0-9]([._+-]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}$");
 		return emailRegex.test(email);	
 	}
+	
+	validateMobileNumber(mobileNumber){
+		let mobileRegex=RegExp("^[0-9]{2}[ ]{1}[0-9]{10}$");
+		return mobileRegex.test(mobileNumber);
+	}
 }
 module.exports=new Validator();
