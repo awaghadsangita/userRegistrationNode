f417e3b4d65a4e8801b9e403514a658090b5c9c2
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-30 10:53:30 +0530

    ADDED:test,should return true if email is valid

--- validatorTest.js
+++ validatorTest.js
@@ -27,3 +27,56 @@ describe('Test for username validity',function(){
 		assert.equal(isValid,false);
 	});
 });
+
+describe('Test for email validity',function(){
+	it('it should return true if mail is valid',function(){
+		let isValid=validator.validateEmail('abc.xyz@bl.co.in');
+		assert.equal(isValid,true);
+	});
+
+	it('it should return true if mail is valid',function(){
+		let isValid=validator.validateEmail('abc@yahoo.com');
+		assert.equal(isValid,true);
+	});
+
+	it('it should return true if mail is valid',function(){
+		let isValid=validator.validateEmail('abc-100@yahoo.com');
+		assert.equal(isValid,true);
+	});
+
+	it('it should return true if mail is valid',function(){
+		let isValid=validator.validateEmail('abc.100@yahoo.com');
+		assert.equal(isValid,true);
+	});
+
+	it('it should return true if mail is valid',function(){
+		let isValid=validator.validateEmail('abc111@abc.com');
+		assert.equal(isValid,true);
+	});
+
+	it('it should return true if mail is valid',function(){
+		let isValid=validator.validateEmail('abc-100@abc.net');
+		assert.equal(isValid,true);
+	});
+
+	it('it should return true if mail is valid',function(){
+		let isValid=validator.validateEmail('abc.100@abc.com.au');
+		assert.equal(isValid,true);
+	});
+
+	it('it should return true if mail is valid',function(){
+		let isValid=validator.validateEmail('abc@1.com');
+		assert.equal(isValid,true);
+	});
+
+	it('it should return true if mail is valid',function(){
+		let isValid=validator.validateEmail('abc@gmail.com.com');
+		assert.equal(isValid,true);
+	});
+
+	it('it should return true if mail is valid',function(){
+		let isValid=validator.validateEmail('abc+100@gmail.com');
+		assert.equal(isValid,true);
+	});
+
+});
